36c5bd92a481ec4be999f3726e3a970ab0c9cc19
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2026-01-18 17:50:59 +0100

    Add all color variants

--- fabric/src/client/java/com/enricobarbieri/glasspanedoormod/GlassPaneDoorModClient.java
+++ fabric/src/client/java/com/enricobarbieri/glasspanedoormod/GlassPaneDoorModClient.java
@@ -8,6 +8,21 @@ public class GlassPaneDoorModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		BlockRenderLayerMap.putBlock(ModBlocks.GLASS_PANE_DOOR, ChunkSectionLayer.TRANSLUCENT);
+		BlockRenderLayerMap.putBlock(ModBlocks.WHITE_GLASS_PANE_DOOR, ChunkSectionLayer.TRANSLUCENT);
+		BlockRenderLayerMap.putBlock(ModBlocks.LIGHT_GRAY_GLASS_PANE_DOOR, ChunkSectionLayer.TRANSLUCENT);
+		BlockRenderLayerMap.putBlock(ModBlocks.GRAY_GLASS_PANE_DOOR, ChunkSectionLayer.TRANSLUCENT);
 		BlockRenderLayerMap.putBlock(ModBlocks.BLACK_GLASS_PANE_DOOR, ChunkSectionLayer.TRANSLUCENT);
+		BlockRenderLayerMap.putBlock(ModBlocks.BROWN_GLASS_PANE_DOOR, ChunkSectionLayer.TRANSLUCENT);
+		BlockRenderLayerMap.putBlock(ModBlocks.RED_GLASS_PANE_DOOR, ChunkSectionLayer.TRANSLUCENT);
+		BlockRenderLayerMap.putBlock(ModBlocks.ORANGE_GLASS_PANE_DOOR, ChunkSectionLayer.TRANSLUCENT);
+		BlockRenderLayerMap.putBlock(ModBlocks.YELLOW_GLASS_PANE_DOOR, ChunkSectionLayer.TRANSLUCENT);
+		BlockRenderLayerMap.putBlock(ModBlocks.LIME_GLASS_PANE_DOOR, ChunkSectionLayer.TRANSLUCENT);
+		BlockRenderLayerMap.putBlock(ModBlocks.GREEN_GLASS_PANE_DOOR, ChunkSectionLayer.TRANSLUCENT);
+		BlockRenderLayerMap.putBlock(ModBlocks.CYAN_GLASS_PANE_DOOR, ChunkSectionLayer.TRANSLUCENT);
+		BlockRenderLayerMap.putBlock(ModBlocks.LIGHT_BLUE_GLASS_PANE_DOOR, ChunkSectionLayer.TRANSLUCENT);
+		BlockRenderLayerMap.putBlock(ModBlocks.BLUE_GLASS_PANE_DOOR, ChunkSectionLayer.TRANSLUCENT);
+		BlockRenderLayerMap.putBlock(ModBlocks.PURPLE_GLASS_PANE_DOOR, ChunkSectionLayer.TRANSLUCENT);
+		BlockRenderLayerMap.putBlock(ModBlocks.MAGENTA_GLASS_PANE_DOOR, ChunkSectionLayer.TRANSLUCENT);
+		BlockRenderLayerMap.putBlock(ModBlocks.PINK_GLASS_PANE_DOOR, ChunkSectionLayer.TRANSLUCENT);
 	}
 }
--- fabric/src/main/java/com/enricobarbieri/glasspanedoormod/ModBlocks.java
+++ fabric/src/main/java/com/enricobarbieri/glasspanedoormod/ModBlocks.java
@@ -18,8 +18,23 @@ import java.util.function.Function;
 import com.enricobarbieri.glasspanedoormod.block.GlassPaneDoorBlock;
 
 public class ModBlocks {
-    public static final Block GLASS_PANE_DOOR = registerGlassPaneDoorBlock("glass_pane_door");;
-    public static final Block BLACK_GLASS_PANE_DOOR = registerGlassPaneDoorBlock("black_glass_pane_door");;
+    public static final Block GLASS_PANE_DOOR = registerGlassPaneDoorBlock("glass_pane_door");
+    public static final Block BLACK_GLASS_PANE_DOOR = registerGlassPaneDoorBlock("black_glass_pane_door");
+    public static final Block RED_GLASS_PANE_DOOR = registerGlassPaneDoorBlock("red_glass_pane_door");
+    public static final Block WHITE_GLASS_PANE_DOOR = registerGlassPaneDoorBlock("white_glass_pane_door");
+    public static final Block LIGHT_GRAY_GLASS_PANE_DOOR = registerGlassPaneDoorBlock("light_gray_glass_pane_door");
+    public static final Block GRAY_GLASS_PANE_DOOR = registerGlassPaneDoorBlock("gray_glass_pane_door");
+    public static final Block BROWN_GLASS_PANE_DOOR = registerGlassPaneDoorBlock("brown_glass_pane_door");
+    public static final Block ORANGE_GLASS_PANE_DOOR = registerGlassPaneDoorBlock("orange_glass_pane_door");
+    public static final Block YELLOW_GLASS_PANE_DOOR = registerGlassPaneDoorBlock("yellow_glass_pane_door");
+    public static final Block LIME_GLASS_PANE_DOOR = registerGlassPaneDoorBlock("lime_glass_pane_door");
+    public static final Block GREEN_GLASS_PANE_DOOR = registerGlassPaneDoorBlock("green_glass_pane_door");
+    public static final Block CYAN_GLASS_PANE_DOOR = registerGlassPaneDoorBlock("cyan_glass_pane_door");
+    public static final Block LIGHT_BLUE_GLASS_PANE_DOOR = registerGlassPaneDoorBlock("light_blue_glass_pane_door");
+    public static final Block BLUE_GLASS_PANE_DOOR = registerGlassPaneDoorBlock("blue_glass_pane_door");
+    public static final Block PURPLE_GLASS_PANE_DOOR = registerGlassPaneDoorBlock("purple_glass_pane_door");
+    public static final Block MAGENTA_GLASS_PANE_DOOR = registerGlassPaneDoorBlock("magenta_glass_pane_door");
+    public static final Block PINK_GLASS_PANE_DOOR = registerGlassPaneDoorBlock("pink_glass_pane_door");
 
     private static Block register(String name, Function<BlockBehaviour.Properties, Block> blockFactory,
             BlockBehaviour.Properties settings, boolean shouldRegisterItem) {
@@ -53,6 +68,51 @@ public class ModBlocks {
         });
         ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register((itemGroup) -> {
             itemGroup.accept(ModBlocks.BLACK_GLASS_PANE_DOOR.asItem());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register((itemGroup) -> {
+            itemGroup.accept(ModBlocks.RED_GLASS_PANE_DOOR.asItem());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register((itemGroup) -> {
+            itemGroup.accept(ModBlocks.WHITE_GLASS_PANE_DOOR.asItem());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register((itemGroup) -> {
+            itemGroup.accept(ModBlocks.LIGHT_GRAY_GLASS_PANE_DOOR.asItem());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register((itemGroup) -> {
+            itemGroup.accept(ModBlocks.GRAY_GLASS_PANE_DOOR.asItem());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register((itemGroup) -> {
+            itemGroup.accept(ModBlocks.BROWN_GLASS_PANE_DOOR.asItem());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register((itemGroup) -> {
+            itemGroup.accept(ModBlocks.ORANGE_GLASS_PANE_DOOR.asItem());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register((itemGroup) -> {
+            itemGroup.accept(ModBlocks.YELLOW_GLASS_PANE_DOOR.asItem());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register((itemGroup) -> {
+            itemGroup.accept(ModBlocks.LIME_GLASS_PANE_DOOR.asItem());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register((itemGroup) -> {
+            itemGroup.accept(ModBlocks.GREEN_GLASS_PANE_DOOR.asItem());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register((itemGroup) -> {
+            itemGroup.accept(ModBlocks.CYAN_GLASS_PANE_DOOR.asItem());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register((itemGroup) -> {
+            itemGroup.accept(ModBlocks.LIGHT_BLUE_GLASS_PANE_DOOR.asItem());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register((itemGroup) -> {
+            itemGroup.accept(ModBlocks.BLUE_GLASS_PANE_DOOR.asItem());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register((itemGroup) -> {
+            itemGroup.accept(ModBlocks.PURPLE_GLASS_PANE_DOOR.asItem());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register((itemGroup) -> {
+            itemGroup.accept(ModBlocks.MAGENTA_GLASS_PANE_DOOR.asItem());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register((itemGroup) -> {
+            itemGroup.accept(ModBlocks.PINK_GLASS_PANE_DOOR.asItem());
         });
     }
 
